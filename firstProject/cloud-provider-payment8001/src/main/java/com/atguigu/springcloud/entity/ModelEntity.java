1c614a6df826371734d1a61baf6acf9a2cc6e602
e <e>
2020-03-12 18:21:37 +0800

    提交远程文件

---
+++ firstProject/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entity/ModelEntity.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.entity;/**
+ * Create by xdd on 2020/3/12.
+ */
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author xdd
+ */
+@Data
+public class ModelEntity
+{
+    private String time;
+    private String hour;
+    private String num;
+}
